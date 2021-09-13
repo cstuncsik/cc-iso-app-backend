eddc30b669e7b3559d3a3eb6cfed2a872d340be2
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2021-09-13 08:28:06 +0200

    update db prod config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,16 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'cc-iso-db'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        host: env('PROD_DATABASE_HOST', '127.0.0.1'),
+        srv: env.bool('PROD_DATABASE_SRV', true),
+        port: env.int('PROD_DATABASE_PORT', 27017),
+        database: env('PROD_DATABASE_NAME', 'cc-iso-db'),
+        username: env('PROD_DATABASE_USERNAME', null),
+        password: env('PROD_DATABASE_PASSWORD', null),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
