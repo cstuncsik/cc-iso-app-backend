eb39db71da4cc966077ea1472f0a8e6b6cbac30a
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2022-12-10 13:56:56 +0100

    add production server config

---
+++ config/env/production/server.ts
@@ -0,0 +1,8 @@
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: env('HOST_URL', 'http://localhost:1337'),
+});
